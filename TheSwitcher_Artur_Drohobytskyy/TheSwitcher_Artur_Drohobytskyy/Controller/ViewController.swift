ec75f77d251b78e1d8b6da0e8f56f5f2a71bc241
@rtur drohobytskyy <artur_drohobytskyy@MacBook-Pro.lan>
2020-01-20 09:27:15 +0000

    base structure setup

---
+++ TheSwitcher_Artur_Drohobytskyy/TheSwitcher_Artur_Drohobytskyy/Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheSwitcher_Artur_Drohobytskyy
+//
+//  Created by @rtur drohobytskyy on 20/01/2020.
+//  Copyright © 2020 @rtur drohobytskyy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
